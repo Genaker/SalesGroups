b172d881e622d9e34625a792d56a97bb9f2b5022
EC2 Default User <ec2-user@ip-172-31-25-45.us-east-2.compute.internal>
2022-02-15 20:10:51 +0000

    first commit

---
+++ OrderSaveBefore.php
@@ -0,0 +1,22 @@
+<?php
+namespace Genaker\SalesGroup;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class OrderSaveBefore implements ObserverInterface
+{
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $order = $observer->getEvent()->getOrder();
+
+        $attr = $order->setData('sales_group_id');
+        $customerId = $order->getCustomerId();
+
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $customerData = $objectManager->get('Magento\Customer\Model\Customer')->load($customerId);
+
+        $salesGroupId = $customerData->getData('sales_group');
+        $order->setData('sales_group_id', $salesGroupId);
+    }
+
+}
